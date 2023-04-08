14ac2754c282016b06babf52fd256432e205095f
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-09 00:30:20 +0530

    Add files via upload

---
+++ 7string.py
@@ -0,0 +1,4 @@
+name="tony Stark"
+
+# print(name.upper()) #here it will give every letter in uppercase but it will not change the original string 
+print(name.find('S'))#here it will find the partiucular character in the string and then it will give the index of that character in which position that index will be 88

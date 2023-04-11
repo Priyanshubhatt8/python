0a2df936565f5d71519d9a4034e0eb66f2fc81bd
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-11 17:02:23 +0530

    Add files via upload

---
+++ 12loops.py
@@ -0,0 +1,27 @@
+#For iteration (see For Loop section) 
+
+#While Loop
+i = 1
+while(i <= 5):
+   print(i)
+   i = i + 1
+
+i = 1
+while(i <= 5):
+   print(i * "*")
+   i = i + 1
+
+i = 5
+while(i >= 1):
+   print(i * "*")
+   i = i - 1
+
+#For Loop (to iterate over a list)
+for i in range(5):
+   print(i)
+   i = i + 1
+
+for i in range(5):
+   print(i * "*")
+   i = i + 1
+

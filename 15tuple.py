f57e696d8939ddc8149ca2e5414c754d1a033e7b
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-12 15:33:11 +0530

    Add files via upload

---
+++ 15tuple.py
@@ -0,0 +1,8 @@
+#Tuples
+#They are like lists (sequence of objects) but they are immutable i.e. once they have been defined we cannot change them.
+#Parenthesis in tuples are optional.
+marks = (95, 98, 97, 97)
+#marks[0] = 98
+
+print(marks.count(97))
+print(marks.index(97))
